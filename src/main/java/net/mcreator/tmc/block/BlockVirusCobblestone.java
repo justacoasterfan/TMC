61a91072decbc8f0f3b12cca6675652254ee8ca8
Finn <Finn@DESKTOP-8N8LR4U.fritz.box>
2021-03-17 16:29:02 +0100

    Virus Stone added aber keine crafting recepies

--- src/main/java/net/mcreator/tmc/block/BlockVirusCobblestone.java
+++ src/main/java/net/mcreator/tmc/block/BlockVirusCobblestone.java
@@ -7,14 +7,9 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 
-import net.minecraft.world.IBlockAccess;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.NonNullList;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.Item;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.Block;
@@ -23,39 +18,34 @@ import net.mcreator.tmc.creativetab.TabTooMuchCommander;
 import net.mcreator.tmc.ElementsToomuchCommander;
 
 @ElementsToomuchCommander.ModElement.Tag
-public class BlockVirusStone extends ElementsToomuchCommander.ModElement {
-	@GameRegistry.ObjectHolder("tmc:virusstone")
+public class BlockVirusCobblestone extends ElementsToomuchCommander.ModElement {
+	@GameRegistry.ObjectHolder("tmc:viruscobblestone")
 	public static final Block block = null;
-	public BlockVirusStone(ElementsToomuchCommander instance) {
-		super(instance, 80);
+	public BlockVirusCobblestone(ElementsToomuchCommander instance) {
+		super(instance, 85);
 	}
 
 	@Override
 	public void initElements() {
-		elements.blocks.add(() -> new BlockCustom().setRegistryName("virusstone"));
+		elements.blocks.add(() -> new BlockCustom().setRegistryName("viruscobblestone"));
 		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
 	}
 
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void registerModels(ModelRegistryEvent event) {
-		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, new ModelResourceLocation("tmc:virusstone", "inventory"));
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, new ModelResourceLocation("tmc:viruscobblestone", "inventory"));
 	}
 	public static class BlockCustom extends Block {
 		public BlockCustom() {
 			super(Material.ROCK);
-			setUnlocalizedName("virusstone");
+			setUnlocalizedName("viruscobblestone");
 			setSoundType(SoundType.STONE);
 			setHardness(2.5F);
 			setResistance(6F);
 			setLightLevel(0F);
 			setLightOpacity(255);
 			setCreativeTab(TabTooMuchCommander.tab);
-		}
-
-		@Override
-		public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
-			drops.add(new ItemStack(BlockVirusCobblestone.block, (int) (1)));
 		}
 	}
 }
